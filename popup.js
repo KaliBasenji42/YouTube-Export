82dd12fcf5060583008a7bc098124b2150e3619c
Jacob D - Home <jacobdelzer369@gmail.com>
2025-05-15 00:42:02 -0600

    Navigation

--- popup.js
+++ popup.js
@@ -2,11 +2,15 @@
 
 let trueRandBox = document.getElementById('trueRand');
 
+let navs = document.querySelectorAll('td.nav');
+let sects = document.getElementsByClassName('sect');
+
 let exportBttn = document.getElementById('export');
 let exportStopBttn = document.getElementById('exportStop');
 let expProgress = document.getElementById('expProgress');
 let expStart = document.getElementById('expStart');
 let expStop = document.getElementById('expStop');
+let expDownload = document.getElementById('expDownload')
 
 // Functions
 
@@ -27,6 +31,31 @@ function send(toAddr, msgSub, msgVal) {
   
   console.log(msg);
   
+}
+
+// Nav events
+
+for(let nav of navs) {
+  nav.addEventListener('click', () => {
+    
+    // Selected
+    
+    for(let n of navs) {
+      n.className = 'nav';
+    }
+    
+    nav.className = 'nav navSel';
+    
+    // Sect
+    
+    for(let sect of sects) {
+      sect.className = 'sect';
+      if(sect.id == nav.id) {
+        sect.className = 'sect navSel';
+      }
+    }
+    
+  });
 }
 
 // Events
